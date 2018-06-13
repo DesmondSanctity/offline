a7162f22ff7ec5b526743f7891bff6c7732981c4
Opara-Prosper <oparaprosper79@gmail.com>
2018-06-13 07:35:12 +0100

    I included the offline source code

---
+++ sw.js
@@ -0,0 +1,44 @@
+let cacheVersion = "cache-v1";
+
+self.addEventListener("install", event => {
+    const urlsToCache = [
+        // include the urls you want to cache
+        // 'dist/index.html',
+        // 'css/index.css'
+    ];
+
+    event.waitUntil(
+        caches.open(cacheVersion).
+        then( cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("activate", event =>{
+    event.waitUntil(
+        caches.keys().
+        then(cahesName => {
+            return Promise.all(
+                cahesName.filter( cacheName => {
+                    return cacheName.startsWith("cache") && cacheName !== cacheVersion;
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).
+        then(response => {
+            if(response){
+                return response;
+            }
+
+            return fetch(event.request);
+        })
+    );
+});
